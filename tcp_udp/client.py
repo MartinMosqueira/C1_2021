0dedc8746a49b8b77becefde301e16dc7d13ab15
MartinMosqueira <tinchomosqueira@gmail.com>
2021-08-29 17:30:07 -0300

    Finalizado tp tcp_udp
    Se termino de realizar las ultimas correcciones del tp de tcp_udp.

--- tcp_udp/client.py
+++ tcp_udp/client.py
@@ -1,11 +1,13 @@
 import socket
 import argparse
+import sys
+import re
 
 
 analizador = argparse.ArgumentParser()
-analizador.add_argument("-w", "--host", help="Host", type=str)
-analizador.add_argument("-p", "--port", help="Puerto", type=int)
-analizador.add_argument("-t", "--transport", help="package transport", type=str)
+analizador.add_argument("-a", help="Host", type=str)
+analizador.add_argument("-p", help="Puerto", type=int)
+analizador.add_argument("-t", help="package transport", type=str)
 argumento = analizador.parse_args()
 
 def tcp_client(host, port):
@@ -14,12 +16,11 @@ def tcp_client(host, port):
     sock.connect(serverAddress)
 
     while True:
-        message = input('user:~$')
+        print('message:'+'\n')
+        message = sys.stdin.read()
         sock.sendall(bytes(message,encoding='utf8'))
-        data = sock.recv(1024)
-        print(data.decode('utf-8'))
 
-        if message == 'exit':
+        if re.search('exit', message):
             break
     print('closing socket')
     sock.close()
@@ -27,21 +28,20 @@ def tcp_client(host, port):
 def udp_client(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-        message = input('user:~$')
+        print('message:'+'\n')
+        message = sys.stdin.read()
         sock.sendto(bytes(message,encoding='utf8'), (host, port))
-        data=sock.recvfrom(1024)
-        print(data[0].decode('utf8'))
 
-        if message == 'exit':
+        if re.search('exit', message):
             break
     print('closing socket')
     sock.close()
 
 if __name__ == "__main__":
-    HOST=argumento.host
-    PORT=argumento.port
+    HOST=argumento.a
+    PORT=argumento.p
 
-    if argumento.transport == 'tcp':
+    if argumento.t == 'tcp':
         tcp_client(HOST, PORT)
-    elif argumento.transport == 'udp':
+    elif argumento.t == 'udp':
         udp_client(HOST, PORT)
--- tcp_udp/server.py
+++ tcp_udp/server.py
@@ -3,10 +3,9 @@ import argparse
 
 
 analizador = argparse.ArgumentParser()
-analizador.add_argument("-w", "--host", help="Host", type=str)
-analizador.add_argument("-p", "--port", help="Port", type=int)
-analizador.add_argument("-t", "--transport", help="package transport", type=str)
-analizador.add_argument("-l", "--file", help="Save file", type=str)
+analizador.add_argument("-p", help="Port", type=int)
+analizador.add_argument("-t", help="package transport", type=str)
+analizador.add_argument("-f", help="Save file", type=str)
 argumento = analizador.parse_args()
 
 def tcp_server(host,port):
@@ -27,9 +26,6 @@ def tcp_server(host,port):
                 if not data:
                     print('no data from', client_address)
                     break
-                
-                msg='received: '+data.decode('utf-8')
-                connection.sendall(bytes(msg,'utf-8'))
                 file.write(data.decode('utf-8')+'\n')
 
         finally:
@@ -46,22 +42,19 @@ def udp_server(host, port):
         print('waiting for a connection')
         data_client, client_address = objectSocket.recvfrom(1024)
         print('connection from ', client_address)
-        msg='received: '+data_client.decode('utf-8')
-        objectSocket.sendto(bytes(msg,'utf-8'), client_address)
         file.write(data_client.decode('utf-8')+'\n')
 
     file.close()
     clientsocket.close()
 
-
 if __name__ == "__main__":
 
-    HOST=argumento.host
-    PORT=argumento.port
-    file=open(argumento.file, 'a')
+    HOST=''
+    PORT=argumento.p
+    file=open(argumento.f, 'a')
 
-    if argumento.transport == 'tcp':
+    if argumento.t == 'tcp':
         tcp_server(HOST, PORT)
-    elif argumento.transport == 'udp':
+    elif argumento.t == 'udp':
         udp_server(HOST, PORT)
     
